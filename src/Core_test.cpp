6ec4e0f5279dfa135e6365823e5d94b90b9727a0
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-09-02 16:04:32 -0300

    Makefile: make test support

---
+++ src/Core_test.cpp
@@ -0,0 +1,33 @@
+/*
+ *   Galeb - Load Balance as a Service Plataform
+ *
+ *   Copyright (C) 2014-2015 Globo.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+
+#define BOOST_TEST_MODULE galeb::Core
+#define BOOST_TEST_DYN_LINK
+
+#include "galeb.hpp"
+#include <boost/test/unit_test.hpp>
+
+namespace galeb {
+
+BOOST_AUTO_TEST_CASE(CoreTest)
+{
+	BOOST_CHECK(true);
+}
+
+} /* namespace galeb */
